e09afcc166a5464f633e3fc6d1f646e818e8769d
Kubing <t6m8@hotmail.com>
2019-03-15 17:17:20 +0800

    init commit

---
+++ APPPInCSharp_TemplateMethodPattern.Console/FtoCRaw.cs
@@ -0,0 +1,25 @@
+﻿namespace APPPInCSharp_TemplateMethodPattern.Console
+{
+    public class FtoCRaw
+    {
+        public void Run()
+        {
+            bool done = false;
+            while (!done)
+            {
+                string fahrString = System.Console.In.ReadLine();
+                if (fahrString == null || fahrString.Length == 0)
+                {
+                    done = true;
+                }
+                else
+                {
+                    double fahr = double.Parse(fahrString);
+                    double celcius = 5.0 / 9.0 * (fahr - 32);
+                    System.Console.Out.WriteLine($"F={fahr}, C={celcius}");
+                }
+            }
+            System.Console.WriteLine("ftoc exit");
+        }
+    }
+}
